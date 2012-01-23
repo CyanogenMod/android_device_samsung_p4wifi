6b845f6d55da6b05dd807d9f37e51464a8acaf4e
Prashant Somashekar <prashsomash@gmail.com>
2012-01-23 13:01:52 -0500

    overlay: add config options removed from common
    -add local overlay to DEVICE_PACKAGE_OVERLAY via .mk
    -change to eth0 for config_datause_iface
    
    Change-Id: Idafc8770e71f5ab181874c1f4bc4c597dbaaae27

--- p4wifi.mk
+++ p4wifi.mk
@@ -33,6 +33,9 @@ $(call inherit-product, device/samsung/p4-common/p4-common.mk)
 
 $(call inherit-product-if-exists, vendor/samsung/p4wifi/p4wifi-vendor.mk)
 
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := p4wifi
 PRODUCT_DEVICE := p4wifi
