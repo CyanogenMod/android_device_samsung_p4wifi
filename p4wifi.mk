d853e6aa4fe9c4d673bf28b7ca0beea4c74af7bb
Prashant Somashekar <prashsomash@gmail.com>
2012-10-14 17:53:28 -0400

    build: add cameradata/hdmi files

--- p4wifi.mk
+++ p4wifi.mk
@@ -35,6 +35,18 @@ $(call inherit-product, device/samsung/p4-common/p4-common.mk)
 
 $(call inherit-product-if-exists, vendor/samsung/p4wifi/p4wifi-vendor.mk)
 
+# Cameradata
+PRODUCT_COPY_FILES += \
+    device/samsung/p4-common/camera/cameradata/back_camera_test_pattern.yuv:system/cameradata/back_camera_test_pattern.yuv \
+    device/samsung/p4-common/camera/cameradata/datapattern_420sp.yuv:system/cameradata/datapattern_420sp.yuv \
+    device/samsung/p4-common/camera/cameradata/datapattern_front_420sp.yuv:system/cameradata/datapattern_front_420sp.yuv \
+    device/samsung/p4-common/camera/cameradata/front_camera_test_pattern.yuv:system/cameradata/front_camera_test_pattern.yuv
+
+# Hdmi
+PRODUCT_COPY_FILES += \
+    device/samsung/p4-common/hdmi/dectable1.dat:system/etc/hdmi/dectable1.dat \
+    device/samsung/p4-common/hdmi/dectable.dat:system/etc/hdmi/dectable.dat
+
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
